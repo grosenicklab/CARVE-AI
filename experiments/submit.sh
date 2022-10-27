8b3652937ca6954e62d0b768cc343707b0201802
Amanda M Buch <amanda.m.buch@gmail.com>
2022-10-26 23:13:53 -0400

    Update submit.sh

--- experiments/submit.sh
+++ experiments/submit.sh
@@ -3,3 +3,15 @@ code_path='/athena/listonlab/store/amb2022/PCMF/'
 echo sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_SRBCT.sh
 sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_SRBCT.sh
 
+echo sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_NCI.sh
+sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_NCI.sh
+
+echo sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_GbmBreastLungCancer.sh
+sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_GbmBreastLungCancer.sh
+
+echo sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_14c.sh
+sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_14c.sh
+
+echo sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_MouseOrgans.sh
+sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_MouseOrgans.sh
+
