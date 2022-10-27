59c1692ce161d9c2ae66ffba8292df5e6c4df6c9
Amanda M Buch <amanda.m.buch@gmail.com>
2022-10-26 23:20:53 -0400

    Create run_genomics_run_MouseOrgans.sh

---
+++ experiments/run_genomics_run_MouseOrgans.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=aistats_genomics_MouseOrgans    # Job name
+#SBATCH --nodes=1                    # Run all processes on a single node	
+#SBATCH --cpus-per-task=10                   # Run 16 tasks	
+#SBATCH --mem=200gb                    # Job memory request
+#SBATCH -p scu-cpu
+pwd; hostname; date
+
+echo 'Submitting python initialize MouseOrgans'
+
+echo 'activating conda environment'
+eval "$(conda shell.bash hook)"
+conda activate jupyter-lab
+
+echo 'Code path is : '${code_path}
+cd $code_path
+pwd
+
+echo python ${code_path}experiments/initialize_genomics_MouseOrgans.py $code_path
+python ${code_path}experiments/initialize_genomics_MouseOrgans.py $code_path
