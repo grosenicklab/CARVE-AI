f33eb7474f84d0f43eed13906d60dfb88e6964a1
Amanda M Buch <amanda.m.buch@gmail.com>
2022-10-26 23:03:01 -0400

    Update submit.sh

--- experiments/submit.sh
+++ experiments/submit.sh
@@ -1,5 +1,5 @@
-AISTATS Genomics
-	code_path='/athena/listonlab/store/amb2022/PCMF/'
-	echo sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_SRBCT.sh.sh
-	sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_SRBCT.sh.sh
+#SRBCT
+code_path='/athena/listonlab/store/amb2022/PCMF/'
+echo sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_SRBCT.sh.sh
+sbatch --export=code_path=$code_path ${code_path}experiments/run_genomics_run_SRBCT.sh.sh
 
